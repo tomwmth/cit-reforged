441dd151225c348c85028b1c2d5b0ff9dfd65c35
Thomas Wearmouth <tomwmth@pm.me>
2024-10-29 09:52:15 +0800

    Port unwrap parity fix

--- src/main/java/dev/tomwmth/citreforged/mixin/defaults/common/TextureAtlasMixin.java
+++ src/main/java/dev/tomwmth/citreforged/mixin/defaults/common/TextureAtlasMixin.java
@@ -1,8 +1,10 @@
-package dev.tomwmth.citreforged.mixin;
+package dev.tomwmth.citreforged.mixin.defaults.common;
 
-import net.minecraft.client.resources.model.AtlasSet;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -11,18 +13,19 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
  * @author Thomas Wearmouth
  * Created on 29/10/2024
  */
-@Mixin(AtlasSet.StitchResult.class)
-public abstract class StitchResultMixin {
-    @Unique private static final String PNG_EXTENSION = ".png";
+@Mixin(TextureAtlas.class)
+public abstract class TextureAtlasMixin {
+    @Shadow @Final private static String FILE_EXTENSION;
+
     @Unique private static final String TEXTURE_DIR = "textures/";
 
     @ModifyVariable(method = "getSprite", argsOnly = true, at = @At("HEAD"))
     private ResourceLocation citresewn$unwrapTexturePaths(ResourceLocation id) {
-        if (id.getPath().endsWith(PNG_EXTENSION)) {
-            id = id.withPath(path -> path.substring(0, path.length() - PNG_EXTENSION.length()));
+        if (id.getPath().endsWith(FILE_EXTENSION)) {
+            id = new ResourceLocation(id.getNamespace(), id.getPath().substring(0, id.getPath().length() - FILE_EXTENSION.length()));
 
             if (id.getPath().startsWith(TEXTURE_DIR)) {
-                id = id.withPath(path -> path.substring(TEXTURE_DIR.length()));
+                id = new ResourceLocation(id.getNamespace(), id.getPath().substring(TEXTURE_DIR.length()));
             }
         }
         return id;
